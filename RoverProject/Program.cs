7a8a7d9a58bdd68094e8a2fecfa0bb8f21e35177
bbezzant998 <bbezzant998@gmail.com>
2021-02-23 16:51:51 -0700

    Main project.

--- RoverProject/Program.cs
+++ RoverProject/Program.cs
@@ -1,21 +1,114 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RoverProject
 {
+
   class Program
   {
+    public const int SIZE_ARGS = 2;
+    public const int POSITION_ARGS = 3;
+
+    public static Rover theRover;
+    public static int xSize, ySize;
+
+    /// <summary>
+    /// Prompt user for the values of the grid size and the position and heading of the rover.
+    ///
+    /// Assumptions:
+    /// User can enter incorrect data.
+    /// Handle missing arguments.
+    /// Handle letters being entered instead of numbers.
+    /// Handle negative numbers. Negative numbers will be converted to positive numbers.
+    /// User can only enter N, E, S, W for heading. Lowercase will be converted to uppercase.
+    /// Handle user entering in a position that is outside the grid size.  
+    /// </summary>
     static void Main(string[] args)
     {
-      // The code provided will print ‘Hello World’ to the console.
-      // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-      Console.WriteLine("Hello World!");
-      Console.ReadKey();
+      string[] xySize;
+      string[] xyPosition;
+      bool invalidData;
 
-      // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+      do
+      {
+        invalidData = false;
+        Console.WriteLine("Please enter x and y for grid size: ");
+        xySize = Console.ReadLine().Split(' ');
+        Console.WriteLine("Please enter the x y position and heading of the rover:");
+        xyPosition = Console.ReadLine().Split(' ');
+        try
+        {
+          ParseData(xySize, xyPosition);
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine(e.Message);
+          invalidData = true;
+        }
+      } while (invalidData);
+
+      EnterCommands();
+    }
+
+    /// <summary>
+    /// Prompt user to enter in a command stream to move the rover around.
+    /// Allow user to enter in values after execution. They can quit by typing q. 
+    ///
+    /// Assumptions:
+    /// User will enter in other values other than L, R, M. Ingore all other data values.
+    /// Lowercase values will be converted to uppercase.
+    /// Handle empty command stream. Set command stream to an empty string if no values are entered. 
+    /// </summary>
+    public static void EnterCommands()
+    {
+      string commandStream;
+      do
+      {
+        Console.WriteLine("Enter in a stream of commands for the Rover to execute (q to quit):");
+        commandStream = Console.ReadLine();
+        commandStream = commandStream.Length > 0 ? commandStream.ToUpper() : "";
+        theRover.ExecuteOrder(commandStream);
+        Console.WriteLine("{0} {1} {2}", theRover.XPos, theRover.YPos, theRover.Direction.ToString()[0]);
+      } while (commandStream.Length > 0 ? char.ToLower(commandStream[0]) != 'q' : true);
+
+    }
+
+    /// <summary>
+    ///Parse string data to integer values.
+    ///Build the rover. Setting its position and heading
+    /// 
+    /// Assumptions:
+    /// Handle negative values.
+    /// Make sure positions are not greater than grid boundaries.
+    /// 
+    /// </summary>
+    public static void ParseData(string[] xySize, string[] xyPosition)
+    {
+      if (xySize.Length == SIZE_ARGS && xyPosition.Length == POSITION_ARGS)
+      {
+        int xPos, yPos;
+        try
+        {
+          xPos = Math.Abs(int.Parse(xyPosition[0]));
+          yPos = Math.Abs(int.Parse(xyPosition[1]));
+          xSize = Math.Abs(int.Parse(xySize[0]));
+          ySize = Math.Abs(int.Parse(xySize[1]));
+
+          if (xPos > xSize || yPos > ySize)
+          {
+            throw new ArgumentOutOfRangeException();
+          }
+
+          theRover = new Rover(xPos, yPos, xSize, ySize, xyPosition[2]);
+        }
+        catch (Exception e)
+        {
+          throw new FormatException(e.Message);
+        }
+      }
+      else
+      {
+        throw new Exception("Missing data arguments for the setup. Check to make sure all data is entered correctly.");
+      }
     }
   }
 }
---
+++ RoverProject/Rover.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace RoverProject
+{
+  /// <summary>
+  /// Use a heading enum for direction instead of using magic numbers. 
+  /// </summary>
+  enum Heading
+  {
+    North = 0,
+    East = 1,
+    South = 2,
+    West = 3
+  }
+  class Rover
+  {
+    public int XPos { get; set; }
+    public int YPos { get; set; }
+    public int XBoundary { get; set; }
+    public int YBoundary { get; set; }
+    public Heading Direction { get; set; }
+    public static int numOfHeadings = Enum.GetNames(typeof(Heading)).Length;
+
+    public Rover(int xPos, int yPos, int xSize, int ySize, string direction)
+    {
+      this.XPos = xPos;
+      this.YPos = yPos;
+      XBoundary = xSize;
+      YBoundary = ySize;
+      this.Direction = CalculateHeading(direction);
+    }
+
+    /// <summary>
+    /// Used to convert the string value of direction to the correct enum heading.
+    ///
+    /// Assumptions:
+    /// Only allow the user to enter N, E, S, W for the direction.
+    /// Throw error prompting user on what the correct heading values are. 
+    /// </summary>
+    public Heading CalculateHeading(string direction)
+    {
+      direction = direction.ToUpper();
+      switch (direction)
+      {
+        case "N":
+          return Heading.North;
+        case "E":
+          return Heading.East;
+        case "S":
+          return Heading.South;
+        case "W":
+          return Heading.West;
+        default:
+          throw new ArgumentException("Heading must be N, E, S or W");
+      }
+    }
+
+    /// <summary>
+    /// Turn the rover left or right depending on the turnDirection. Right adds, left subtracts
+    /// 
+    /// Assumptions:
+    /// Handle negative direction. If turning makes the direction negative then it has gone from North to West.
+    /// Handle values greater than number of directions. Moding direction after adding or subtracting a value will
+    /// keep the direction value within the values of the heading enum. 
+    /// </summary>
+    public void Turn(char turnDirection)
+    {
+      int direction = (int)this.Direction;
+      direction += turnDirection == 'R' ? 1 : -1;
+      direction = direction < 0 ? (int)Heading.West : direction % numOfHeadings;
+      this.Direction = (Heading)direction;
+    }
+
+    /// <summary>
+    /// Move the rover and upadte its position depending on the direction.
+    ///
+    /// Assumptions:
+    /// Handle movement that will move the rover outside the boundaries.
+    /// Throw an exeception with the current heading of the rover. 
+    /// </summary>
+    public void Move()
+    {
+      try
+      {
+        switch (Direction)
+        {
+          case Heading.North:
+            if (YPos + 1 > YBoundary) throw new ArgumentOutOfRangeException("N");
+            YPos++;
+            break;
+          case Heading.East:
+            if (XPos + 1 > XBoundary) throw new ArgumentOutOfRangeException("E");
+            XPos++;
+            break;
+          case Heading.South:
+            if (YPos - 1 < 0) throw new ArgumentOutOfRangeException("S");
+            YPos--;
+            break;
+          case Heading.West:
+            if (XPos - 1 < 0) throw new ArgumentOutOfRangeException("W");
+            XPos--;
+            break;
+        }
+      }
+      catch (ArgumentOutOfRangeException e)
+      {
+        Console.WriteLine("\nTrying to head {0}", e.ParamName);
+        Console.WriteLine("\nWARNING!!! Heading this direction, while at the rover's current position, will cause the rover to fall off the cliff and explode. Costing your company Millions of Dollars!!!");
+        Console.WriteLine("\nFor your job security, command was not executed.");
+      }
+    }
+
+    /// <summary>
+    /// Go through the command stream entered by the user and execute the orders.
+    ///
+    /// Assumption:
+    /// Ignore all other values that are not L, R, M.
+    /// </summary>
+    public void ExecuteOrder(string orders)
+    {
+      foreach (var order in orders)
+      {
+        if (order == 'R' || order == 'L')
+        {
+          Turn(order);
+        }
+        else if (order == 'M')
+        {
+          Move();
+        }
+      }
+
+    }
+
+  }
+}
